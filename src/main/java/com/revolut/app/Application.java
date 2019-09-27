b6229d563870cbc91360f220c815778e06802632
verevic <vveretenov@inbox.ru>
2019-09-27 16:40:03 +0300

    Add swagger docs

--- src/main/java/com/revolut/app/Application.java
+++ src/main/java/com/revolut/app/Application.java
@@ -1,7 +1,18 @@
 package com.revolut.app;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "account-service",
+                version = "0.1",
+                description = "Account service API",
+                license = @License(name = "Apache 2.0")
+        )
+)
 public class Application {
 
     public static void main(String[] args) {
